c4639d0ed9701e3b68d3a99cf5e95c0fb6e91c8d
Egor <esmirnov1234@gmail.com>
2026-01-07 14:16:32 +0300

    refactor: Update library interface and main example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
+pub mod data;
 pub mod nn;
 pub mod ops;
 pub mod optim;
+pub mod read_csv;
 pub mod tensor;
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,35 @@
-use ndarray::prelude::*;
-use ru_torch::nn::{Linear, Module, ReLU, Sequential};
-use ru_torch::optim::{Optimizer, SGD};
-use ru_torch::tensor::Tensor;
+use ndarray::{Array2, Axis, s};
+use ru_torch::{
+    data::{DataLoader, TensorDataset},
+    nn::{Linear, Module, Sequential, Tanh},
+    optim::{Optimizer, SGD},
+    read_csv::csv_to_array2,
+    tensor::Tensor,
+};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let model = Sequential::new(vec![
-        Box::new(Linear::new(2, 3, true)),
-        Box::new(ReLU),
-        Box::new(Linear::new(3, 4, true)),
+        Box::new(Linear::new(2, 4, true)),
+        Box::new(Tanh),
+        Box::new(Linear::new(4, 1, true)),
     ]);
 
-    let mut optimizer = SGD::new(&model, 0.01);
+    let mut optimizer = SGD::new(&model, 0.05);
 
-    let inputs = vec![
-        array![[0.0, 0.0]],
-        array![[0.0, 1.0]],
-        array![[1.0, 0.0]],
-        array![[1.0, 1.0]],
-    ];
+    let array: Array2<f64> = csv_to_array2("xor.csv")?;
 
-    let targets = vec![
-        array![[0.0, 0.0, 0.0, 0.0]],
-        array![[1.0, 1.0, 1.0, 1.0]],
-        array![[1.0, 1.0, 1.0, 1.0]],
-        array![[2.0, 2.0, 2.0, 2.0]],
-    ];
+    let inputs: Array2<f64> = array.slice(s![.., 0..2]).to_owned();
 
-    let epochs = 500;
+    let targets: Array2<f64> = array.column(2).to_owned().insert_axis(Axis(1));
 
-    for epoch in 0..epochs {
+    let dataset = TensorDataset::new(inputs, targets);
+    let mut dataloader = DataLoader::new(dataset).batch_size(4).shuffle(true);
+
+    for epoch in 0..1000 {
         let mut total_loss = 0.0;
+        let mut num_batches = 0;
 
-        for (x_data, y_data) in inputs.iter().zip(&targets) {
-            let x = Tensor::new(x_data.clone());
-            let target = Tensor::new(y_data.clone());
-
+        for (x, target) in dataloader.iter() {
             let output = model.forward(&x);
 
             let diff = Tensor::sub(&output, &target);
@@ -48,14 +43,17 @@ fn main() {
             optimizer.zero_grad();
 
             total_loss += loss.borrow().data[[0, 0]];
+            num_batches += 1;
         }
 
         if epoch % 100 == 0 {
             println!(
                 "Epoch {} | Loss: {:.6}",
                 epoch,
-                total_loss / inputs.len() as f64
+                total_loss / num_batches as f64
             );
         }
     }
+
+    Ok(())
 }
